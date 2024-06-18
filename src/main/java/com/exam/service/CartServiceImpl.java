335aa75718d237539b31ba920b6c54ddbaf9ae4d
hyunahparc <hyunah.parc@gmail.com>
2024-06-18 17:40:20 +0900

    상품 장바구니 담기 구현

---
+++ src/main/java/com/exam/service/CartServiceImpl.java
@@ -0,0 +1,30 @@
+package com.exam.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.exam.dto.CartDTO;
+import com.exam.mapper.CartMapper;
+
+@Service
+public class CartServiceImpl implements CartService {
+	
+	// CartMapper와 연동하기 (생성자 이용)
+	CartMapper cartMapper;
+	
+	public CartServiceImpl(CartMapper cartMapper) {
+		this.cartMapper = cartMapper;
+	}
+
+	@Override
+	public int cartAdd(CartDTO dto) {
+		return cartMapper.cartAdd(dto);
+	}
+
+	@Override
+	public List<CartDTO> cartList(String userid) {
+		return cartMapper.cartList(userid);
+	}
+
+}
